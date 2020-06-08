4d2d70052b0374a473bc3de3915e894006e0aa08
Eric Gorski <er.h.gorski@gmail.com>
2020-06-08 11:53:01 +0200

    renaming files for heroku deployment

--- src/logoBig.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function logoBig() {
-    return <img height="120" src="/logo.png" />;
-}
--- src/logoSmall.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function logoSmall() {
-    return <img height="70" src="/logo.png" />;
-}
